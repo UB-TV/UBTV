9469ebfe6545d3c2ab18e9f2dd0b5e6573e00a65
Mirza Hilmi <exquisitemirza@student.ub.ac.id>
2023-11-04 17:27:48 +0700

    fix: Fix nix dev environment keep getting gc-ed

--- flake.nix
+++ flake.nix
@@ -2,23 +2,24 @@
   description = "UBTV's dev environment";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {nixpkgs, ...} @ inputs: let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs {
-      inherit system;
-      overlays = [(import ./overlays.nix {inherit inputs;}).unstable-packages];
-    };
-  in {
-    devShells.${system}.default = pkgs.mkShell {
-      packages = with pkgs; [
-        php83
-        php83Packages.composer
-        unstable.nodejs_20
-      ];
-    };
-  };
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          php83
+          php83Packages.composer
+          nodejs_20
+        ];
+      };
+    });
 }
