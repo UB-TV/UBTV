bed429879649a28b22b18ebe34a1670b763c234b
Mirza Hilmi <exquisitemirza@student.ub.ac.id>
2024-02-07 12:58:26 +0700

    fix: Fix incorrect flake inputs url schema

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "UBTV's dev environment";
 
   inputs = {
-    nixpkgs.url = "github.com:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
