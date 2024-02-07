dbd08a4834d28434069cf9c30db21c1ff065a849
Mirza Hilmi <exquisitemirza@student.ub.ac.id>
2024-02-07 13:09:16 +0700

    fix: Fix error on nixpkgs overlay

--- flake.nix
+++ flake.nix
@@ -6,18 +6,12 @@
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    ...
-  } @ inputs: let
+  outputs = {nixpkgs, ...} @ inputs: let
     system = "x86_64-linux";
-
-    nixpkgs = {
+    pkgs = import nixpkgs {
+      inherit system;
       overlays = [(import ./overlays.nix {inherit inputs;}).unstable-packages];
     };
-
-    pkgs = import nixpkgs {inherit system;};
   in {
     devShells.${system}.default = pkgs.mkShell {
       packages = with pkgs; [
