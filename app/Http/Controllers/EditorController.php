217830db5bb13af0ea7d840fc78c32db35a59731
Mirza Hilmi <dev@mrz.my.id>
2024-09-29 20:08:31 +0700

    feat: upload segment api

--- app/Http/Controllers/EditorController.php
+++ app/Http/Controllers/EditorController.php
@@ -2,64 +2,47 @@
 
 namespace App\Http\Controllers;
 
+use Google\Client;
 use Inertia\Inertia;
+use App\Models\Video;
 use Inertia\Response;
 use App\Models\Program;
+use Google\Service\Drive;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Google\Service\Drive\DriveFile;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Database\Query\JoinClause;
+use App\Http\Requests\PostEpisodeSegmentRequest;
 
 class EditorController extends Controller
 {
     public function uploaded(Request $req): Response
     {
         $user = $req->user();
-        $raw = DB::table('programs')
-            ->select(['programs.*', DB::raw('sum(episodes.segment_count) as segment_total'), DB::raw('count(*) as video_count')])
+        $programs = Program::query()
             ->join('episodes', 'programs.id', '=', 'episodes.program_id')
             ->join('videos', function (JoinClause $join) {
-                $join->on('episodes.id', '=', 'videos.episode_id');
-                $join->on('episodes.segment_count', 'is', DB::raw('not null'));
+                $join->on('episodes.id', '=', 'videos.episode_id')
+                    ->whereNotNull('episodes.segment_count');
             })
             ->join('user_video', 'videos.id', '=', 'user_video.video_id')
-            ->where('user_video.user_id', '=', $user->id)
             ->groupBy('programs.id')
-            ->paginate()
-            ->onEachSide(15);
-        $filtered = $raw->filter(function ($data, $key) {
-            return (int)$data->segment_total === $data->video_count;
-        });
-        $programs = $filtered->transform(function ($data, $key) {
-            unset($data->segment_total, $data->video_count);
-            return $data;
-        });
+            ->get();
         dd(json_encode($programs));
         #TODO: render the correct page & delete dd
         return Inertia::render('CHANGEME', $programs);
     }
 
-    // Lazy implementation, fix later
     public function notUploaded(Request $req): Response
     {
         $user = $req->user();
-        $raw = DB::table('programs')
-            ->select(['programs.*', DB::raw('sum(episodes.segment_count) as segment_total'), DB::raw('count(*) as video_count')])
+        $programs = Program::query()
             ->join('episodes', 'programs.id', '=', 'episodes.program_id')
-            ->join('videos', function (JoinClause $join) {
-                $join->on('episodes.id', '=', 'videos.episode_id');
-            })
-            ->join('user_video', 'videos.id', '=', 'user_video.video_id')
-            ->where('user_video.user_id', '=', $user->id)
+            ->leftJoin('videos', 'episodes.id', '=', 'videos.episode_id')
+            ->whereNull('videos.id')
             ->groupBy('programs.id')
-            ->paginate()
-            ->onEachSide(15);
-        $filtered = $raw->filter(function ($data, $key) {
-            return (int)$data->segment_total === $data->video_count;
-        });
-        $programs = $filtered->transform(function ($data, $key) {
-            unset($data->segment_total, $data->video_count);
-            return $data;
-        });
+            ->get();
         dd(json_encode($programs));
         #TODO: render the correct page & delete dd
         return Inertia::render('CHANGEME', $programs);
@@ -73,15 +56,15 @@ class EditorController extends Controller
             ->select('videos.*')
             ->join('episodes', 'videos.episode_id', '=', 'episodes.id')
             ->where('episodes.program_id', '=', $program->id)
-            ->orderBy('programs.id', 'asc')
+            ->orderBy('episodes.program_id', 'asc')
             ->orderBy('videos.segment_number', 'asc')
             ->get();
         $uploadedSegments = [];
         for ($i = 0, $j = 0; $i < $raw->count(); $i++) {
+            $uploadedSegments[$j][] = $raw[$i];
             if (isset($raw[$i + 1]) && $raw[$i + 1]->episode_id != $raw[$i]->episode_id) {
                 ++$j;
             }
-            $uploadedSegments[$j][] = $raw[$i];
         }
         dd(json_encode([
             'program' => $program,
@@ -94,5 +77,25 @@ class EditorController extends Controller
             'episodes' => $episodes,
             'episode_with_segments' => $uploadedSegments,
         ]);
+    }
+
+    public function upload(PostEpisodeSegmentRequest $req): RedirectResponse
+    {
+        $payload = $req->validated();
+        $client = new Client();
+        $client->useApplicationDefaultCredentials();
+        client->addScope(Drive::DRIVE);
+        $drive = new Drive($client);
+        $metadata = new DriveFile(['name' => $payload['attachment']->getClientOriginalName()]);
+        $file = $this->driveService->files->create($metadata, [
+            'data' => $payload['attachment'],
+            'fields' => 'id',
+        ]);
+        Video::create([
+            'episode_id' => $payload['episode_id'],
+            'object_id' => $file->id,
+            'segment_number' => $payload['segment_number'],
+        ]);
+        return back();
     }
 }
