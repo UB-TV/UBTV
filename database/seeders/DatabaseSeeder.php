42fc7ffc185e2e1291de1675ef8990516da57a37
Mirza Hilmi <dev@mrz.my.id>
2024-12-16 19:05:19 +0700

    feat(mcr): add notifications api

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,8 +7,10 @@ use App\Models\Video;
 use App\Models\Episode;
 use App\Models\Program;
 use App\Enums\StatusEnum;
+use App\Models\Notification;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -82,6 +84,25 @@ class DatabaseSeeder extends Seeder
                     ]);
                 }
             }
+        }
+
+        $users = User::all();
+        $programs = Program::all();
+        $roles = Role::all();
+
+        foreach ($users as $user) {
+            // Randomly assign programs and episodes to notifications
+            $program = $programs->random();
+            $episode = $program->episodes->random();
+            $role = $roles->random();
+
+            Notification::create([
+                'user_id' => $user->id,
+                'program_id' => $program->id,
+                'episode_id' => $episode->id,
+                'role_id' => $role->id,
+                'message' => "Notification for user {$user->name} on program {$program->name}",
+            ]);
         }
     }
 }
