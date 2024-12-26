48bce884e05d771db1c60e97bce4fc8acc3dce05
mamelilea <achmalpradiptaaditama@gmail.com>
2024-12-27 02:02:15 +0700

    `fix:` set empty string in user_video dropIfExists

--- database/migrations/2024_09_06_074330_create_user_video_table.php
+++ database/migrations/2024_09_06_074330_create_user_video_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-return new class () extends Migration {
+return new class() extends Migration {
     /**
      * Run the migrations.
      */
@@ -27,6 +27,6 @@ return new class () extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('user_video');
+        Schema::dropIfExists('');
     }
 };
